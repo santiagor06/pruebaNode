d047a309f87abb47867b753eb08c655ba4e4320d
santiagor06 <santiago.restrepo06@hotmail.com>
2023-05-06 15:28:27 -0500

    first commit

---
+++ API/src/index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const { parseSing, parseSingUpdate, parseId } = require("./services/parseSing");
+const app = express();
+app.use(express.json());
+var SING = [];
+
+const PORT = 3001;
+app.listen(PORT);
+
+app.get("/", (req, res) => {
+  res.status(200).send(SING);
+});
+app.post("/", (req, res) => {
+  try {
+    const newSing = parseSing(req.body);
+    const id = SING.length + 1;
+    SING.push({ id, ...newSing });
+    res.status(200).send("Sing created");
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+app.put("/", (req, res) => {
+  try {
+    const id = parseId(req.body.id);
+    SING = SING.filter((s) => id !== s.id);
+    const singUpdate = parseSingUpdate(req.body);
+    SING.push(singUpdate);
+    res.status(200).send("Sing Updated");
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+app.delete("/", (req, res) => {
+  try {
+    const id = parseId(req.body.id);
+    SING = SING.filter((s) => s.id !== id);
+    res.status(200).send(`Sing ${id} deleted`);
+  } catch (error) {
+    send.status(400).send(error.message);
+  }
+});
+console.log(`server running ${PORT}`);
---
+++ API/src/services/parseSing.js
@@ -0,0 +1,64 @@
+const isString = (param) => {
+  if (typeof param === "string") return true;
+  else throw new Error("must be a string");
+};
+
+const isNumber = (param) => {
+  if (typeof param === "number") return true;
+  else throw new Error("must be a number");
+};
+
+const parseName = (param) => {
+  if (!param) throw new Error("Enter a Name");
+  else if (isString(param)) return param;
+  else throw new Error("Invalid Name");
+};
+
+const parseDate = (param) => {
+  if (!param) throw new Error("Enter a Date");
+  else if (isString(param)) return param;
+  else throw new Error("Invalid Date");
+};
+const parseBiography = (param) => {
+  if (!param) throw new Error("Enter a Biography");
+  else if (isString(param)) return param;
+  else throw new Error("Invalid Biography");
+};
+
+const parsePhoto = (param) => {
+  if (!param) throw new Error("Enter a URL Photo");
+  else if (isString(param)) return param;
+  else throw new Error("Invalid URL");
+};
+const parseGenere = (param) => {
+  if (!param) throw new Error("Enter a Genere");
+  else if (isString(param)) return param;
+  else throw new Error("Invalid Genere");
+};
+
+const parseId = (param) => {
+  if (!param) throw new Error("Enter a Id");
+  else if (isNumber(param)) return param;
+  else throw new Error("Invalid Id");
+};
+const parseSing = (param) => {
+  return {
+    name: parseName(param.name),
+    date: parseDate(param.date),
+    biography: parseBiography(param.biography),
+    photo: parsePhoto(param.photo),
+    genere: parseGenere(param.genere),
+  };
+};
+const parseSingUpdate = (param) => {
+  return {
+    id: parseId(param.id),
+    name: parseName(param.name),
+    date: parseDate(param.date),
+    biography: parseBiography(param.biography),
+    photo: parsePhoto(param.photo),
+    genere: parseGenere(param.genere),
+  };
+};
+
+module.exports = { parseSing, parseSingUpdate, parseId };
